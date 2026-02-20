ac8603ec34eb5fa6fce7bb5cc3d96e0120498dca
Rogue <rogue87@tuta.io>
2026-02-20 23:46:55 +0200

    remove trailing slash from url

--- src/lib/site-config.ts
+++ src/lib/site-config.ts
@@ -4,4 +4,4 @@ export const title = "Den";
 export const desc = "I like doing random things :)";
 export const author = "Rogue87";
 export const email = "rogue87@tuta.io";
-export const url = dev ? "http://localhost:5173" : "https://rogue87.gitlab.io/";
+export const url = dev ? "http://localhost:5173" : "https://rogue87.gitlab.io";
