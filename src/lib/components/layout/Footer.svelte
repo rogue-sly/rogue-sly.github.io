88f8853541cee2a87cfa56a4fdf161f9e01e6fe3
Rogue <rogue87@tuta.io>
2026-02-24 17:30:07 +0200

    update footer component links

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,18 +1,19 @@
 <footer>
     <p>
-        music provided by <a href="https://nightride.fm">nightride.fm</a>.
+        music brought to you by <a href="https://nightride.fm" target="_blank">nightride.fm</a>.
     </p>
     <p>
         icons are licensed under the
-        <a href="https://en.wikipedia.org/wiki/MIT_License">MIT license</a>.
+        <a href="https://en.wikipedia.org/wiki/MIT_License" target="_blank">MIT license</a>.
     </p>
     <p>
-        font name is "Quantico", and is licensed under
-        <a href="https://openfontlicense.org/open-font-license-official-text/">open font license</a>.
+        both "Quantico" and "JetBrains Mono" fonts are licensed under
+        <a href="https://openfontlicense.org/open-font-license-official-text/" target="_blank">open font license</a
+        >.
     </p>
     <p>
         consider other content is licensed under the
-        <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>.
+        <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0</a>.
     </p>
 </footer>
 
