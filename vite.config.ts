8f267a7601570a4c7ebc1909de84fd1365a53fc6
Rogue <rogue87@tuta.io>
2026-04-01 13:11:02 +0200

    fix a very weird and disgustingly long svelte-check error

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 export default defineConfig({
     plugins: [sveltekit(), devtoolsJson()],
     test: { include: ["tests/**/*.{test,spec}.{js,ts}"] },
-    build: {
-        target: "esnext",
-        minify: "esbuild",
-        rollupOptions: {
-            output: {
-                manualChunks(id: string) {
-                    if (id.includes("node_modules/shiki")) {
-                        return "shiki";
-                    }
-
-                    if (id.includes("node_modules/svelte") || id.includes("node_modules/@sveltejs")) {
-                        return "svelte-vendor";
-                    }
-                },
-            },
-        },
-    },
     esbuild: { treeShaking: true },
     optimizeDeps: { include: ["svelte", "@sveltejs/kit"] },
 });
