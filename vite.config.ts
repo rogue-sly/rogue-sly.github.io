0d57531864d6850b591a26ac278edce09e7c4505
Rogue <rogue87@tuta.io>
2026-04-28 11:02:44 +0200

    manage packages
    - remove vitest
    - update to latest available packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { sveltekit } from "@sveltejs/kit/vite";
 export default defineConfig({
     assetsInclude: ["**/*.cast"],
     plugins: [sveltekit(), devtoolsJson()],
-    // @ts-expect-error
-    test: { include: ["tests/**/*.{test,spec}.{js,ts}"] },
     esbuild: { treeShaking: true },
     optimizeDeps: { include: ["svelte", "@sveltejs/kit"] },
 });
